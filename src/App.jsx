e342aea5f44d2e31cccfd6c635d8ee5b821fc5ed
Zakia Fazzi <kia.fazzi@gmail.com>
2024-11-19 23:18:20 +0100

    first commit and final

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Profile from './components/profile.jsx';
+function App() {
+  return (
+    <div>
+      <Profile></Profile>
+    </div>
+  );
+
+}
+export default App;
---
+++ src/components/profile.jsx
@@ -0,0 +1,50 @@
+import earringsBeige from '../assets/earrings_beige.jpg';
+import earringsBlack from '../assets/earrings_black.jpg';
+import earringsSphere from '../assets/earrings_sphere.jpg';
+import earringsBronze from '../assets/earrings_bronze.jpg';
+import earringsLong from '../assets/earrings_long.jpg';
+import earringsPink from '../assets/earrings_pink.jpg';
+
+
+function Gallery() {
+    return (
+        <div className="gallery">
+            <div className="row">
+                <div className="imageCard">
+                    <img src={earringsBeige} alt="Beige Petal Earrings" className="img" />
+                    <h2 className="imgHeader">Beige Petal Earrings</h2>
+                    <p className="pricetag">$29.99</p>
+                </div>
+                <div className="imageCard">
+                    <img src={earringsBlack} alt="Black Drop Earrings" className="img" />
+                    <h2 className="imgHeader">Black Drop Earrings</h2>
+                    <p className="pricetag">$24.99</p>
+                </div>
+                <div className="imageCard">
+                    <img src={earringsSphere} alt="Gold Sphere Earrings" className="img" />
+                    <h2 className="imgHeader">Gold Sphere Earrings</h2>
+                    <p className="pricetag">$14.99</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="imageCard">
+                    <img src={earringsBronze} alt="Rose Drop Earrings" className="img" />
+                    <h2 className="imgHeader">Rose Drop Earrings</h2>
+                    <p className="pricetag">$29.99</p>
+                </div>
+                <div className="imageCard">
+                    <img src={earringsLong} alt="Pink Longer Earrings" className="img" />
+                    <h2 className="imgHeader">Pink Longer Earrings</h2>
+                    <p className="pricetag">$34.99</p>
+                </div>
+                <div className="imageCard">
+                    <img src={earringsPink} alt="Pink Petal Earrings" className="img" />
+                    <h2 className="imgHeader">Pink Petal Earrings</h2>
+                    <p className="pricetag">$39.99</p>
+                </div>
+            </div>
+        </div>
+      );
+}
+
+export default Gallery;
