6d61187485679d2adbc8bd275ddefe59bdc6e36f
Zakia Fazzi <kia.fazzi@gmail.com>
2024-11-24 23:19:17 +0100

    Added navbar and some css

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import Gallery from '../components/profile.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from '../pages/home.jsx';
+import About from '../pages/about.jsx';
+import Navbar from '../navbar.jsx';
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+            <Navbar></Navbar>
+                <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+export default App;
---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import Profile from '../components/profile.jsx';
+function About() {
+  return (
+    <div>
+      <Profile></Profile>
+    </div>
+  );
+
+}
+export default About;
