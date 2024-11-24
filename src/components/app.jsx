414f31f95808175b261b240da1c41b1c57e0340b
Zakia Fazzi <kia.fazzi@gmail.com>
2024-11-25 00:21:10 +0100

    Added a footer and changed some margins

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,17 +4,19 @@ import Home from '../pages/home.jsx';
 import About from '../pages/about.jsx';
 import Jewelry from '../pages/jewelry.jsx';
 import Navbar from '../navbar.jsx';
+import Footer from '../footer.jsx';
 function App() {
     return (
         <>
             <BrowserRouter>
             <Navbar></Navbar>
                 <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/about" element={<About/>} />
-                <Route path="/jewelry" element={<Jewelry/>} />
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/jewelry" element={<Jewelry/>} />
                 </Routes>
             </BrowserRouter>
+            <Footer></Footer>
         </>
     );
 }
---
+++ src/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="footer-box">
+        <p className="footer-text">Copyright &copy;{new Date().getFullYear()} Fazzi Jewelry. All rights reserved.</p>
+    </div>
+  );
+
+}
+export default Footer;
