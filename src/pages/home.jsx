034df42ff643f3ff55d2e4bbcb3c69c6eb7c55dc
Zakia Fazzi <kia.fazzi@gmail.com>
2024-11-25 21:49:11 +0100

    Added the increment, decrement and reset button on the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,34 @@
-function Home () {
+import { useState } from "react";
 
-    const handleEvent = () => {
-        console.log('Button clicked!');
-    }
+function Home() {
+  const [count, setCount] = useState(0);
 
-    return (
-    <div> 
-        <button className="home-btn" onClick={handleEvent}>Click here</button>
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter">
+      <h1>Counter: {count}</h1>
+      <button className="increment-btn" onClick={increment}>
+        Increment
+      </button>
+      <button className="decrement-btn" onClick={decrement}>
+        Decrement
+      </button>
+      <button className="reset-btn" onClick={reset}>
+        Reset
+      </button>
     </div>
-    );
+  );
 }
+
 export default Home;
